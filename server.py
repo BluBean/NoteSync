9559e73445b1d528aed17da35ab025c07355f98d
lmckay1727 <93024589+lmckay1727@users.noreply.github.com>
2021-11-16 20:41:17 -0700

    Update server.py
    added options and help

--- server.py
+++ server.py
@@ -354,20 +354,43 @@ mainwindow.config(menu=main_menu)
 mainwindow.config(background='gray')
 
 #menu commands
-def new_command():
+"""def new_command():
     file_path0 = filedialog.askopenfilename()
 def save_command():
     file_path = filedialog.asksaveasfilename()
-    file_selected = 1
+    file_selected = 1"""
+def help():
+    tutorial = Tk()
+    tutorial.title("Notesync User Guide")
+    tutorial.iconbitmap("NoteSync_icon.ico")
+    tutorial.geometry("700x500")
+    gotit = Button(tutorial, text="Thanks for the help!", command=tutorial.destroy)
+    text_widget = Text(tutorial) #, height=400, width=40
+    scroll_bar = Scrollbar(tutorial)
+
+    scroll_bar.pack(side=RIGHT, fill="y", expand=False)
+    text_widget.pack(side=LEFT, fill="both", expand=True)
+    text_widget.config(yscrollcommand=scroll_bar.set)
+    long_text = """This is a multiline string.
+    We can write this in multiple lines too!
+    Hello from the Ass master Lance. This is the third line.
+    This is the fourth line. Although the length of the text is longer than
+    the width, we can use tkinter's scrollbar to solve this problem!
+    """
+    text_widget.insert(END, long_text)
+    text_widget.configure(state='disabled')
+    gotit.pack(side=BOTTOM)
+
 
 #create new menu options
 
 
 
 file_menu = Menu(main_menu)
-main_menu.add_cascade(label="File",menu=file_menu)
-file_menu.add_command(label="New...", command=new_command)
-file_menu.add_command(label="Save Location", command=save_command)
+main_menu.add_cascade(label="Options",menu=file_menu)
+#file_menu.add_command(label="New...", command=new_command)
+#file_menu.add_command(label="Save Location", command=save_command)
+file_menu.add_command(label="Help", command=help)
 file_menu.add_command(label="Authors", command=authors)
 file_menu.add_command(label="Exit", command=mainwindow.quit)
 #BPM and time signature slider scale
